32a51610441a20dc50bbe63403a76a9069c8ab5c
snake <snake@cosyjoy.com>
2018-05-29 17:10:58 +0800

    UGUI  碰撞边界处理

---
+++ NetWork/Assets/Scripts/UGUI/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerMove : MonoBehaviour
+{
+    RectTransform selfTransform;
+
+    public RectTransform SelfTransform
+    {
+        get
+        {
+            if (selfTransform == null)
+            {
+                selfTransform = GetComponent<RectTransform>();
+            }
+            return selfTransform;
+        }
+    }
+
+    private void Start()
+    {
+        OnMove();
+    }
+    public Vector2 dic = Vector2.zero;
+    public void OnMove()
+    {
+
+    }
+    public void OnMove(Vector2 ve2)
+    {
+        dic = ve2;
+    }
+    public void OnMove(float x, float y)
+    {
+        dic = new Vector2(x, y);
+    }
+
+    private void Update()
+    {
+        SelfTransform.Translate(dic * Time.deltaTime);
+    }
+}
